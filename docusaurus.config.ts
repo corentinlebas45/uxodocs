25428c38c987435ab57fab11c1bb181b77805802
Corentin LEBAS <corentin.lebas@arondor.com>
2025-10-24 10:11:40 +0200

    fix config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
       'classic',
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: require.resolve('./sidebars.ts'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
